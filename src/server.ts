2c558673dd46804072aed1927a8592136e5ce503
JeyllonSandoval <jeyllomsandoval@gmail.com>
2025-01-30 23:13:06 -0400

    feat: run server

--- src/server.ts
+++ src/server.ts
@@ -3,13 +3,13 @@ import Fastify from "fastify";
 const fastify = Fastify({ logger: true });
 
 fastify.get("/", async (request, reply) => {
-    return { message: "¡Hola, Fastify!" };
+    return { message: "¡Hello, Fastify!. This is a new era" };
 });
 
 const start = async () => {
     try {
         await fastify.listen({ port: 3000, host: "0.0.0.0" });
-        console.log("Servidor corriendo en http://localhost:3000");
+        console.log("Server run in http://localhost:3000");
     } catch (err) {
         fastify.log.error(err);
         process.exit(1);
