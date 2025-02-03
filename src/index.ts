16b6c1a24c26e3a5ee0007fc3190d8302cb1fbbf
JeyllonSandoval <jeyllomsandoval@gmail.com>
2025-02-02 22:22:33 -0400

    feat: connect database

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import Fastify from "fastify";
+import 'dotenv/config';
+import db from "./db/db";
 
 const fastify = Fastify({ logger: true });
 
-fastify.get("/", async (request, reply) => {
+fastify.get("/", async (_request) => {
     return { message: "¡Hello, Fastify!. This is a new era" };
 });
 
@@ -10,7 +12,8 @@ const start = async () => {
     try {
         await fastify.listen({ port: 3000, host: "0.0.0.0" });
         fastify.log.info(`Server listening on ${fastify.server.address()}`);
-
+        db
+        fastify.log.info(`Database connected`);
     } catch (err) {
         fastify.log.error(err);
         process.exit(1);
