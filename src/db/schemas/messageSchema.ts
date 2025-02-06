ee2a06f2dc06ce56c719f6d611fa5b74b064ef43
20200966 <20200966@miucateci.edu.do>
2025-02-06 13:38:30 -0400

    feat: create messageSchema

---
+++ src/db/schemas/messageSchema.ts
@@ -0,0 +1,14 @@
+import { sql } from "drizzle-orm";
+import { sqliteTable, text } from "drizzle-orm/sqlite-core";
+import chatTable from "./chatSchema";
+
+const messageTable = sqliteTable("message_table", {
+    MessageID: text("MessageID", { length: 36 }).primaryKey(),
+    ChatID: text("ChatID", {length: 36}).notNull().references(()=> chatTable.ChatID),
+    sender_type: text("sender_type").notNull(),
+    content: text("content").notNull(),
+    createdAt: text("createdAt").default(sql `(CURRENT_TIMESTAMP)`).notNull(),
+    status: text("status").notNull(),
+});
+
+export default messageTable;
