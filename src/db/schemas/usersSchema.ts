a5c33fd80174f1d9ddc89421d91ddb809c76775b
JeyllonSandoval <jeyllomsandoval@gmail.com>
2025-02-06 00:02:05 -0400

    feat: Create UserSchema

--- src/db/schemas/usersSchema.ts
+++ src/db/schemas/usersSchema.ts
@@ -1,10 +1,17 @@
-import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
-export const usersTable = sqliteTable("users_table", {
-    id: int().primaryKey({ autoIncrement: true }),
-    name: text().notNull(),
-    lastname: text().notNull(),
-    age: int().notNull(),
-    email: text().notNull().unique(),
-    password: text().notNull(),
+import { sql } from "drizzle-orm";
+import { sqliteTable, text } from "drizzle-orm/sqlite-core";
+
+const usersTable = sqliteTable("users_table", {
+    UserID: text("UserID", {length: 36}).primaryKey(),
+    imageUser: text("imageUser").notNull(),
+    FirstName: text("FirstName").notNull(),
+    LastName: text("LastName").notNull(),
+    CountryID: text("CountryID", {length: 36}).notNull(),
+    Email: text("Email").notNull().unique(),
+    password: text("password", {length: 60}).notNull(),
+    createdAt: text("createdAt").default(sql `(CURRENT_TIMESTAMP)`).notNull(),
+    status: text("status").notNull(),
 });
+
+export default usersTable;
