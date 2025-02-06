253cde18d7b9a065b6a05856f49c380d0f7bcd7c
20200966 <20200966@miucateci.edu.do>
2025-02-06 13:02:19 -0400

    feat: create countrySchema

--- src/db/schemas/usersSchema.ts
+++ src/db/schemas/usersSchema.ts
@@ -1,6 +1,7 @@
 import rolesTable from "./rolesSchema";
 import { sql } from "drizzle-orm";
 import { sqliteTable, text } from "drizzle-orm/sqlite-core";
+import countryTable from "./countrySchema";
 
 const usersTable = sqliteTable("users_table", {
     UserID: text("UserID", {length: 36}).primaryKey(),
@@ -8,7 +9,7 @@ const usersTable = sqliteTable("users_table", {
     imageUser: text("imageUser").notNull(),
     FirstName: text("FirstName").notNull(),
     LastName: text("LastName").notNull(),
-    CountryID: text("CountryID", {length: 36}).notNull(),
+    CountryID: text("CountryID", {length: 36}).notNull().references(()=> countryTable.CountryID),
     Email: text("Email").notNull().unique(),
     password: text("password", {length: 60}).notNull(),
     createdAt: text("createdAt").default(sql `(CURRENT_TIMESTAMP)`).notNull(),
