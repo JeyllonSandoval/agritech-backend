5e1f4998174aaa4f1d6fbe77f235454aed1aa010
20200966 <20200966@miucateci.edu.do>
2025-02-06 13:11:17 -0400

    feat: create fileSchema

---
+++ src/db/schemas/filesScema.ts
@@ -0,0 +1,13 @@
+import { sql } from "drizzle-orm";
+import { sqliteTable, text } from "drizzle-orm/sqlite-core";
+import usersTable from "./usersSchema";
+
+const filesTable = sqliteTable("files_table", {
+    FileID: text("FileID", { length: 36 }).primaryKey(),
+    contentURL: text("contentURL").notNull(),
+    UserID: text("UserID", {length: 36}).notNull().references(()=> usersTable.UserID),
+    createdAt: text("createdAt").default(sql `(CURRENT_TIMESTAMP)`).notNull(),
+    status: text("status").notNull(),
+});
+
+export default filesTable;
