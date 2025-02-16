1f40c94afd1f2ce0ff0c07dcd327d999a2c0f612
JeyllonSandoval <jeyllomsandoval@gmail.com>
2025-02-16 02:05:38 -0400

    fix: show messages with ChatID

--- src/routers/message.routes.ts
+++ src/routers/message.routes.ts
@@ -1,9 +1,10 @@
 import { FastifyInstance } from "fastify";
-import { createMessage, getMessages } from "@/controllers/message";
+import { createMessage, getMessages, getChatMessages } from "@/controllers/message";
 
 const messageRoutes = async (fastify: FastifyInstance) => {
     fastify.post("/message", createMessage);
     fastify.get("/messages", getMessages);
+    fastify.get("/messages/:ChatID", getChatMessages);
 }
 
 export default messageRoutes;
