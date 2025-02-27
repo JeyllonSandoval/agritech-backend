edf1389ccf7feaab8281d62338f1d833dc8381bc
JeyllonSandoval <jeyllomsandoval@gmail.com>
2025-02-27 16:42:57 -0400

    fix: export roleSchema

--- src/libs/InitialSetup.ts
+++ src/libs/InitialSetup.ts
@@ -1,10 +1,10 @@
-import db from "@/db/db";
-import rolesTable from "@/db/schemas/rolesSchema";
+import db from "../db/db";
+import rolesSchema from "../db/schemas/rolesSchema";
 import { v4 as uuidv4 } from "uuid";
 
 const createRoles = async () => {
     try {
-        const count = await db.select().from(rolesTable);
+        const count = await db.select().from(rolesSchema);
 
         if (count.length > 0) {
             console.log("Roles already exist in the database ✅");
@@ -16,7 +16,7 @@ const createRoles = async () => {
             { rolename: "admin", status: "active" }
         ].map(role => ({ ...role, RoleID: uuidv4() }));
 
-        await db.insert(rolesTable).values(roles);
+        await db.insert(rolesSchema).values(roles);
 
         console.log("Created roles successfully ✅");
     } catch (error) {
