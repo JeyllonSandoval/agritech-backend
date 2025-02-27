6470b18c67df5365370e09500fd5ab46ad022f2a
JeyllonSandoval <jeyllomsandoval@gmail.com>
2025-02-27 15:06:35 -0400

    fix: index config

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,10 @@ import createRoles from "./libs/InitialSetup";
 import multipart from "@fastify/multipart";
 import { validateCloudinaryConnection } from "./db/services/cloudinary";
 
-const fastify = Fastify({ logger: true });
+const fastify = Fastify({ 
+    logger: true,
+    disableRequestLogging: true
+});
 
 // Configuración de multipart
 fastify.register(multipart);
@@ -28,10 +31,13 @@ createRoles();
 
 const start = async () => {
     try {
-
         await validateCloudinaryConnection();
 
-        await fastify.listen({ port: 5000, host: "0.0.0.0" });
+        const port = process.env.PORT || 5000;
+        await fastify.listen({ 
+            port: parseInt(port.toString()), 
+            host: "0.0.0.0" 
+        });
         fastify.log.info(`Server listening on ${fastify.server.address()}`);
         if (db) {
             fastify.log.info("Database connected ✅");
@@ -44,4 +50,11 @@ const start = async () => {
     }
 };
 
-start();
+export default async (req: any, res: any) => {
+    await fastify.ready();
+    fastify.server.emit('request', req, res);
+};
+
+if (process.env.NODE_ENV !== 'production') {
+    start();
+}
