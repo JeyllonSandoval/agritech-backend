0b74b1743d419ddc9d133366b4196c22b8e47095
JeyllonSandoval <jeyllomsandoval@gmail.com>
2025-03-12 23:09:33 -0400

    fix: vercel prod

--- dist/db/schemas/usersSchema.js
+++ dist/db/schemas/usersSchema.js
@@ -3,10 +3,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const rolesSchema_1 = __importDefault(require("@/db/schemas/rolesSchema"));
+const rolesSchema_1 = __importDefault(require("../schemas/rolesSchema"));
 const drizzle_orm_1 = require("drizzle-orm");
 const sqlite_core_1 = require("drizzle-orm/sqlite-core");
-const countrySchema_1 = __importDefault(require("@/db/schemas/countrySchema"));
+const countrySchema_1 = __importDefault(require("../schemas/countrySchema"));
 const usersTable = (0, sqlite_core_1.sqliteTable)("users_Table", {
     UserID: (0, sqlite_core_1.text)("UserID").primaryKey(),
     RoleID: (0, sqlite_core_1.text)("RoleID").notNull().references(() => rolesSchema_1.default.RoleID),
