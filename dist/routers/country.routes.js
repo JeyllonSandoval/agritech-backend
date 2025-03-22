1b3b81432376b716f5e6a1c16b7f5109e44c514f
JeyllonSandoval <jeyllomsandoval@gmail.com>
2025-03-22 10:44:18 -0400

    feat: get country by ID

--- dist/routers/country.routes.js
+++ dist/routers/country.routes.js
@@ -4,5 +4,6 @@ const country_1 = require("@/controllers/country");
 const countryRoutes = async (fastify) => {
     fastify.get("/countries", country_1.getCountries);
     fastify.post("/countries", country_1.createCountry);
+    fastify.get("/countries/:CountryID", country_1.getCountryByID);
 };
 exports.default = countryRoutes;
--- src/controllers/country.ts
+++ src/controllers/country.ts
@@ -3,6 +3,7 @@ import db from "@/db/db";
 import countriesTable from "@/db/schemas/countrySchema";
 import { v4 as uuidv4 } from "uuid";
 import { z, ZodError } from "zod";
+import { eq } from "drizzle-orm";
 
 const getCountries = async (_req: FastifyRequest, reply: FastifyReply) => {
     try {
@@ -63,4 +64,22 @@ const createCountry = async (
     }
 }
 
-export { getCountries, createCountry };
+const getCountryByID = async (req: FastifyRequest<{ Params: { CountryID: string } }>, reply: FastifyReply) => {
+    try {
+        const { CountryID } = req.params;
+
+        const country = await db.select().from(countriesTable).where(eq(countriesTable.CountryID, CountryID));
+
+        if (!country) {
+            return reply.status(404).send({ error: "Country not found" });
+        }
+
+        return reply.status(200).send({ countryname: country[0].countryname });
+        
+    } catch (error) {
+        console.error(error);
+        return reply.status(500).send({ error: "Missing Failed: could not get country by ID" });
+    }
+}
+
+export { getCountries, createCountry, getCountryByID };
