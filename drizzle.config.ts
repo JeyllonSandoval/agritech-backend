8cb41bde7397d13d6ac4a243640af52123e48b47
JeyllonSandoval <jeyllomsandoval@gmail.com>
2025-04-18 18:47:42 -0400

    feat: new db

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,7 +4,7 @@ export default {
     dialect: "sqlite",
     driver: "turso",
     dbCredentials: {
-        url: "https://agritech-db-jey-slon.turso.io",
-        authToken: `eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3Mzg5NzYzMzIsImlkIjoiNTRkODlmOTYtNmI4MS00ZGQwLWJjODYtMjkzOGIzODI0ODBkIn0.uIJrtK-01UzV_j_NbQbOaPeqwIuCQMLbli6NXmvmZbscKFh9KKCmx8q13HLPzKJqHiqNYgZxoYul6fsrpwtpDw`
+        url: process.env.TURSO_DATABASE_URL,
+        authToken: process.env.TURSO_AUTH_TOKEN
     }
 };
